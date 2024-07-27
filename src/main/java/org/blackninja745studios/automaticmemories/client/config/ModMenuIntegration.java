1b063020587d624b18df018c1105e804eb125d48
BlackNinja745 Studios <bn745studiosmanagement@gmail.com>
2024-07-27 16:07:24 -0500

    Change config screen to use subcategories

--- src/main/java/org/blackninja745studios/automaticmemories/client/config/ModMenuIntegration.java
+++ src/main/java/org/blackninja745studios/automaticmemories/client/config/ModMenuIntegration.java
@@ -14,6 +14,7 @@ import org.blackninja745studios.automaticmemories.client.PeriodicTimerSingleton;
 
 import java.io.File;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Optional;
 
 public class ModMenuIntegration implements ModMenuApi {
@@ -26,98 +27,103 @@ public class ModMenuIntegration implements ModMenuApi {
                     .setParentScreen(parent);
 
             ConfigEntryBuilder entryBuilder = builder.entryBuilder();
-            ConfigCategory intervalCategory = builder.getOrCreateCategory(Text.translatable("automaticmemories.config.interval.category"));
 
-            intervalCategory.addEntry(
-                entryBuilder.startLongField(Text.translatable("automaticmemories.config.interval.interval_ms"), Configuration.INTERVAL_MS)
-                    .setDefaultValue(3600 * 1000)
-                    .setMin(5 * 1000)
-                    .setSaveConsumer(l -> {
-                        Configuration.INTERVAL_MS = l;
-                        LogManager.getLogger(AutomaticMemories.class).warn("set new delay");
-                        PeriodicTimerSingleton.restartOrStartTimer(0, l);
-                    })
-                    .setTooltipSupplier(l -> {
-                        Text main = Text.translatable("automaticmemories.config.interval.interval_ms.tooltip.main");
+            ConfigCategory category = builder.getOrCreateCategory(Text.literal("Automatic Screenshot Configuration"));
 
-                        Text current = Text.translatable("automaticmemories.config.interval.interval_ms.tooltip.editing", PeriodicTimerSingleton.formatTime(l))
-                                .formatted(Formatting.GOLD);
+            category.addEntry(
+                entryBuilder.startSubCategory(Text.translatable("automaticmemories.config.interval.category"), List.of(
+                    entryBuilder.startLongField(Text.translatable("automaticmemories.config.interval.interval_ms"), Configuration.INTERVAL_MS)
+                        .setDefaultValue(3600 * 1000)
+                        .setMin(5 * 1000)
+                        .setSaveConsumer(l -> {
+                            Configuration.INTERVAL_MS = l;
+                            LogManager.getLogger(AutomaticMemories.class).warn("set new delay");
+                            PeriodicTimerSingleton.restartOrStartTimer(0, l);
+                        })
+                        .setTooltipSupplier(l -> {
+                            Text main = Text.translatable("automaticmemories.config.interval.interval_ms.tooltip.main");
 
-                        Text remaining = Text.translatable("automaticmemories.config.interval.interval_ms.tooltip.remaining",
-                                PeriodicTimerSingleton.formatTime(Configuration.INTERVAL_MS - PeriodicTimerSingleton.timeSinceLastScreenshot()),
-                                PeriodicTimerSingleton.formatTime(Configuration.INTERVAL_MS)
-                        ).formatted(Formatting.GRAY);
+                            Text current = Text.translatable("automaticmemories.config.interval.interval_ms.tooltip.editing", PeriodicTimerSingleton.formatTime(l))
+                                    .formatted(Formatting.GOLD);
 
-                        return l == Configuration.INTERVAL_MS ?
-                                Optional.of(new Text[] { main, remaining }) :
-                                Optional.of(new Text[] { main, current, remaining });
-                    })
-                    .build()
+                            Text remaining = Text.translatable("automaticmemories.config.interval.interval_ms.tooltip.remaining",
+                                    PeriodicTimerSingleton.formatTime(Configuration.INTERVAL_MS - PeriodicTimerSingleton.timeSinceLastScreenshot()),
+                                    PeriodicTimerSingleton.formatTime(Configuration.INTERVAL_MS)
+                            ).formatted(Formatting.GRAY);
+
+                            return l == Configuration.INTERVAL_MS ?
+                                    Optional.of(new Text[] { main, remaining }) :
+                                    Optional.of(new Text[] { main, current, remaining });
+                        })
+                        .build(),
+
+                    entryBuilder.startBooleanToggle(Text.translatable("automaticmemories.config.interval.restart_timer_each_session"), Configuration.RESTART_TIMER_EACH_SESSION)
+                        .setDefaultValue(false)
+                        .setSaveConsumer(b -> Configuration.RESTART_TIMER_EACH_SESSION = b)
+                        .setTooltip(Optional.of(new Text[] {
+                                Text.translatable("automaticmemories.config.interval.restart_timer_each_session.tooltip")
+                        }))
+                        .build()
+                ))
+                .setExpanded(true)
+                .build()
             );
 
-            intervalCategory.addEntry(
-                entryBuilder.startBooleanToggle(Text.translatable("automaticmemories.config.interval.restart_timer_each_session"), Configuration.RESTART_TIMER_EACH_SESSION)
-                    .setDefaultValue(false)
-                    .setSaveConsumer(b -> Configuration.RESTART_TIMER_EACH_SESSION = b)
-                    .setTooltip(Optional.of(new Text[] {
-                        Text.translatable("automaticmemories.config.interval.restart_timer_each_session.tooltip")
-                    }))
-                    .build()
-            );
+            category.addEntry(
+                entryBuilder.startSubCategory(Text.translatable("automaticmemories.config.save.category"), List.of(
+                    entryBuilder.startTextField(Text.translatable("automaticmemories.config.save.save_directory"), Configuration.SAVE_DIRECTORY)
+                        .setDefaultValue("screenshots")
+                        .setSaveConsumer(s -> Configuration.SAVE_DIRECTORY = s)
+                        .setErrorSupplier(s -> {
+                            try {
+                                Paths.get(s);
+                            } catch (Exception e) {
+                                return Optional.of(Text.translatable("automaticmemories.config.save.save_directory.error", e.getMessage()));
+                            }
+                            return Optional.empty();
+                        })
+                        .setTooltipSupplier(s -> {
+                            File runDir = MinecraftClient.getInstance().runDirectory;
 
-            ConfigCategory saveCategory = builder.getOrCreateCategory(Text.translatable("automaticmemories.config.save.category"));
+                            Text main = Text.translatable("automaticmemories.config.save.save_directory.tooltip.main");
 
-            saveCategory.addEntry(
-                entryBuilder.startTextField(Text.translatable("automaticmemories.config.save.save_directory"), Configuration.SAVE_DIRECTORY)
-                    .setDefaultValue("screenshots")
-                    .setSaveConsumer(s -> Configuration.SAVE_DIRECTORY = s)
-                    .setErrorSupplier(s -> {
-                        try {
-                            Paths.get(s);
-                        } catch (Exception e) {
-                            return Optional.of(Text.translatable("automaticmemories.config.save.save_directory.error", e.getMessage()));
-                        }
-                        return Optional.empty();
-                    })
-                    .setTooltipSupplier(s -> {
-                        File runDir = MinecraftClient.getInstance().runDirectory;
-
-                        Text main = Text.translatable("automaticmemories.config.save.save_directory.tooltip.main");
-
-                        Text current = Text.translatable("automaticmemories.config.save.save_directory.tooltip.editing", Configuration.getFullDirectory(runDir, s))
+                            Text current = Text.translatable("automaticmemories.config.save.save_directory.tooltip.editing", Configuration.getFullDirectory(runDir, s))
                                 .formatted(Formatting.GOLD);
 
-                        Text remaining = Text.translatable("automaticmemories.config.save.save_directory.tooltip.current",
+                            Text remaining = Text.translatable("automaticmemories.config.save.save_directory.tooltip.current",
                                 Configuration.getFullDirectory(runDir, Configuration.SAVE_DIRECTORY)
-                        ).formatted(Formatting.GRAY);
+                            ).formatted(Formatting.GRAY);
 
-                        return Configuration.SAVE_DIRECTORY.equals(s) ?
+                            return Configuration.SAVE_DIRECTORY.equals(s) ?
                                 Optional.of(new Text[] { main, remaining }) :
                                 Optional.of(new Text[] { main, current, remaining });
-                    })
-                    .build()
+                        })
+                        .build(),
+
+                        entryBuilder.startTextField(Text.translatable("automaticmemories.config.save.screenshot_prefix"), Configuration.SCREENSHOT_PREFIX)
+                            .setDefaultValue("auto_")
+                            .setSaveConsumer(s -> Configuration.SCREENSHOT_PREFIX = s)
+                            .setTooltip(Optional.of(new Text[] {
+                                Text.translatable("automaticmemories.config.save.screenshot_prefix.tooltip")
+                            }))
+                            .build()
+                ))
+                .setExpanded(true)
+                .build()
             );
 
-            saveCategory.addEntry(
-                entryBuilder.startTextField(Text.translatable("automaticmemories.config.save.screenshot_prefix"), Configuration.SCREENSHOT_PREFIX)
-                    .setDefaultValue("auto_")
-                    .setSaveConsumer(s -> Configuration.SCREENSHOT_PREFIX = s)
-                    .setTooltip(Optional.of(new Text[] {
-                        Text.translatable("automaticmemories.config.save.screenshot_prefix.tooltip")
-                    }))
-                    .build()
-            );
-
-            ConfigCategory miscCategory = builder.getOrCreateCategory(Text.translatable("automaticmemories.config.miscellaneous.category"));
-
-            miscCategory.addEntry(
-                entryBuilder.startBooleanToggle(Text.translatable("automaticmemories.config.miscellaneous.notify_player"), Configuration.NOTIFY_PLAYER)
-                    .setSaveConsumer(b -> Configuration.NOTIFY_PLAYER = b)
-                    .setDefaultValue(false)
-                    .setTooltip(Optional.of(new Text[] {
-                        Text.translatable("automaticmemories.config.miscellaneous.notify_player")
-                    }))
-                    .build()
+            category.addEntry(
+                entryBuilder.startSubCategory(Text.translatable("automaticmemories.config.miscellaneous.category"), List.of(
+                    entryBuilder.startBooleanToggle(Text.translatable("automaticmemories.config.miscellaneous.notify_player"), Configuration.NOTIFY_PLAYER)
+                        .setSaveConsumer(b -> Configuration.NOTIFY_PLAYER = b)
+                        .setDefaultValue(false)
+                        .setTooltip(Optional.of(new Text[] {
+                            Text.translatable("automaticmemories.config.miscellaneous.notify_player")
+                        }))
+                        .build()
+                ))
+                .setExpanded(true)
+                .build()
             );
 
             return builder.build();
